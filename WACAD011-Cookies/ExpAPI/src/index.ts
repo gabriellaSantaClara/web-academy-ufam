a85fb8cde0c5106c87c9a0a9606138e261389b21
Gabriella Mendonça Santa Clara <31377559+gabriellaSantaClara@users.noreply.github.com>
2025-07-01 20:04:36 -0400

    Delete WACAD011-Cookies directory

--- WACAD011-Cookies/ExpAPI/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import router from './router/v1Router';
-
-const app = express();
-app.use(express.json());
-app.use('/v1', router);
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
--- WACAD011-Cookies/ExpAPI/src/router/v1Router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import productRouter from '../resources/product/product.router';
-
-const router = Router();
-router.use('/product', productRouter);
-export default router;
